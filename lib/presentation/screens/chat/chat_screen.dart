5f76dfde85d069cee1d60809255fda5c3666922c
MendozaEmerson <emendozahi@unsa.edu.pe>
2025-02-17 10:42:41 -0500

    Diseño de la presentacion de mensajes propios.

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_mssg_bubble.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -30,10 +31,11 @@ class _ChatView extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Column(
           children: [
-            Expanded(child: ListView.builder(
+            Expanded(
+                child: ListView.builder(
               itemCount: 120,
               itemBuilder: (context, index) {
-                return Text("Indice: $index");
+                return const MyMssgBubble();
               },
             )),
             Text("End Screen")
